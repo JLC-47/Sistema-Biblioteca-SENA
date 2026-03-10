580792c49deffeb85cd18aaf9373d95a3de6af83
Janier Leudo <janierleudo@gmail.com>
2026-03-10 11:50:34 -0500

    se crean los atributos de la clase libros

--- src/models/Libros.java
+++ src/models/Libros.java
@@ -1,6 +1,13 @@
 package models;
 
 public  class Libros {
+    private String documento;
+    private String nombre;
+    private String  tipoUsuario;
+    private int edad;
 
+    public Libros(String documento, String nombre, String tipoUsuario, int edad){
+        
+    }
     
 }
