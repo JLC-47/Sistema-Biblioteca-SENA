2783783a267a43f459937d3dc1a9a6c836346d8f
Janier Leudo <janierleudo@gmail.com>
2026-03-16 16:51:18 -0500

    Feat: Se hacen las validaciones de la clase libro, falta reunirce con el equipo para validar que este en los parametros establecidos para la el funcionamiento del programa o aplicación con la que se esta trabajando

--- src/utils/Validador.java
+++ src/utils/Validador.java
@@ -2,5 +2,29 @@ package utils;
 
 public class Validador {
 
+    public void validarISBN(int cod){
+        int ISBN = cod;
+        if (ISBN == cod) {
+            System.out.println("No se puede repetir el ISBN de un libro");
+        }
+    }
+
+
+    public void validarAnio(int anio){
+        if (anio > 2026) {
+            System.out.println("No se pude terner un año mayor al actual");
+        }else{
+            System.out.println("Sucess");
+        }
+    }
+
+    public void validarCampo(String campo){
+        if (campo == "") {
+            System.out.println("Ningún campo puede estar vacío.");
+        }else{
+            System.out.println("Sucess");
+        }
+    }
+
     
 }
