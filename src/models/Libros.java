0facf20aa8c1632276d243aaf6386ec2770b5cfe
Janier Leudo <janierleudo@gmail.com>
2026-03-16 16:22:35 -0500

    feat: Se crea la clase libro con todos sus atributos y con los metodos (getters and setters)

--- src/models/Libros.java
+++ src/models/Libros.java
@@ -1,13 +1,57 @@
 package models;
 
 public  class Libros {
-    private String documento;
-    private String nombre;
-    private String  tipoUsuario;
-    private int edad;
+    private int ISBN;
+    private String titulo;
+    private String  autor;
+    private int anioPublicacion;
+    private String  estado;
 
-    public Libros(String documento, String nombre, String tipoUsuario, int edad){
+    public Libros(int ISBN, String titulo, String autor, int edanioPublicacionad, String  estado, int anioPublicacion){
+        this.ISBN = ISBN;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anioPublicacion = anioPublicacion;
+        this.estado = estado;
         
     }
+
+    public int getISBN() {
+        return ISBN;
+    }
+    public void setISBN(int iSBN) {
+        ISBN = iSBN;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+    public void setAnioPublicacion(int anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+
+
     
 }
